4fab14ebf634bad78a7ac4eb218da73ad7c33023
jayjaykai <kevinkai1229@gmail.com>
2024-06-03 20:44:48 +0800

    modify env path in Linux

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ from mysql.connector.pooling import MySQLConnectionPool
 from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
-# app.mount("/static", StaticFiles(directory="static"), name="static")
-app.mount("/static", StaticFiles(directory="/home/ubuntu/tdt/static"), name="static")
-# load_dotenv()
-load_dotenv('/home/ubuntu/tdt/.env')
+app.mount("/static", StaticFiles(directory="static"), name="static")
+# app.mount("/static", StaticFiles(directory="/home/ubuntu/tdt/static"), name="static")
+load_dotenv()
+# load_dotenv('/home/ubuntu/tdt/.env')
 # 設定可存取資源的來源端點
 origins = [
     "http://127.0.0.1:8000",
